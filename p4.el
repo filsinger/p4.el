filsinger/p4.el

commit 11a4ddcf5df0a3c69083ed0e27b542353a6d130e

Show output of "p4 set" in response to failed connection. This will help the user if their Perforce configuration is the cause of the problem. Fixes #122.

p4.el
@@ -870,6 +870,13 @@ To set the executable for future sessions, customize
           "The authenticity of '.*' can't be established")
   "Regular expression matching output from an untrusted Perforce server.")
 
+(defvar p4-connect-failed-regexp
+  (concat "\\(?:error: \\)?"
+          "Perforce client error:\n"
+          "\tConnect to server failed")
+  "Regular expression matching output from Perforce when it can't
+connect to the server.")
+
 (defun p4-request-trust ()
   "Ask the user for permission to trust the Perforce server."
   (display-buffer (current-buffer))
@@ -961,7 +968,13 @@ If there's no content in the buffer, pass `args' to error instead."
            (kill-buffer (current-buffer))
            (error message)))
         (t
-         (with-selected-window (display-buffer (current-buffer))
+         (let ((set (p4-with-set-output
+                      (buffer-substring (point-min) (point-max)))))
+           (with-selected-window (display-buffer (current-buffer))
+             (goto-char (point-max))
+             (if (string-match "\\S-" set)
+                 (insert "\n\"p4 set\" shows that you have the following Perforce configuration:\n" set)
+               (insert "\n\"p4 set\" shows that you have no Perforce configuration.\n")))
            (goto-char (point-min)))
          (apply 'error args))))
 

commit 18cd4ca990a783dcb343c7e106662da7cd9348b6

Make sure to move to beginning of error message. (Fixes bug introduced in commit 11a4ddcf5df0a3c69083ed0e27b542353a6d130e.)

p4.el
@@ -974,8 +974,8 @@ If there's no content in the buffer, pass `args' to error instead."
              (goto-char (point-max))
              (if (string-match "\\S-" set)
                  (insert "\n\"p4 set\" shows that you have the following Perforce configuration:\n" set)
-               (insert "\n\"p4 set\" shows that you have no Perforce configuration.\n")))
-           (goto-char (point-min)))
+               (insert "\n\"p4 set\" shows that you have no Perforce configuration.\n"))
+             (goto-char (point-min))))
          (apply 'error args))))
 
 (defun p4-process-finished (buffer process-name message)